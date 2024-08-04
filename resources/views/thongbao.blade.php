11872fae43bd9428e23d83eaa31d64a298c5302a
hongnxpd09347 <hongnxpd09347@fpt.edu.vn>
2024-08-04 22:15:04 +0700

    upload

---
+++ resources/views/thongbao.blade.php
@@ -0,0 +1,2 @@
+    <h1>403</h1>
+    <p>Bạn không có quyền truy cập trang này</p>
--- routes/web.php
+++ routes/web.php
@@ -6,15 +6,36 @@ use App\Http\Controllers\TinController;
 use App\Http\Controllers\QTinController;
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\ThongtinController;
+use App\Http\Controllers\QuanTriTinController;
 
 Route::get('/', [TinController::class, 'index']);
-Route::get('/tin/{id}', [TinController::class, 'chitiet']);
-Route::get('/cat/{id}', [TinController::class, 'tintrongloai']);
+// Route::get('/tin/{id}', [TinController::class, 'chitiet']);
+// Route::get('/cat/{id}', [TinController::class, 'tintrongloai']);
 
+Route::get('/tin', [QTinController::class, 'index']);
+Route::get('/tin/them', [QTinController::class, 'create']);
+Route::post('/tin/them', [QTinController::class, 'store']);
+Route::delete('/tin/{id}', [QTinController::class, 'destroy']);
+Route::get('/tin/sua/{id}', [QTinController::class, 'edit']);
+Route::post('/tin/sua/{id}', [QTinController::class, 'update']);
+
+Route::get('download', function () {
+    return view("download");
+})->middleware('auth');
+
+Route::get('quantri', function () {
+    return view("quantri");
+})->middleware('auth', 'QuanTri');
+
+Route::get('/quantritin', [QuanTriTinController::class, 'index']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/thongbao', function () {
+    return view("thongbao");
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -22,7 +43,7 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
 
 Auth::routes();
 
